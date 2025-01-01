c84380cbe031d436fe601f37cbfed81b67a916a0
Julkin George <julkingeorge8@gmail.com>
2025-01-01 14:43:40 +0100

    re

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import "./projects.css"
-import { useNavigate,Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 function Projects() {
-const navigator=useNavigate()
+
 
   return (
 
